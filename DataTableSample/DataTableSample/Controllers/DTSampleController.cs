30da96598be2b95e46577fa59a1d09ab90d94b5e
halityurttas <halityurttas@gmail.com>
2016-08-13 01:45:13 +0300

    Create mvc for datatable

---
+++ DataTableSample/DataTableSample/Controllers/DTSampleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataTableSample.Controllers
+{
+    public class DTSampleController : Controller
+    {
+        // GET: DTSample
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
